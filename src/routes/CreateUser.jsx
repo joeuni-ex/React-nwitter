682ad81a4a63e888f5c480c30af323ab1bed0b23
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2023-12-08 10:47:59 +0900

    유저가입

--- src/routes/CreateUser.jsx
+++ src/routes/CreateUser.jsx
@@ -1,7 +1,119 @@
-import React from "react";
+import { useState } from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 420px;
+  padding: 50px 0px;
+`;
+
+const Title = styled.h1`
+  font-size: 42px;
+`;
+
+const Form = styled.form`
+  margin-top: 50px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  width: 100%;
+`;
+
+const Input = styled.input`
+  padding: 10px 20px;
+  border-radius: 50px;
+  border: none;
+  width: 100%;
+  font-size: 16px;
+  &[type="submit"] {
+    cursor: pointer;
+    &:hover {
+      opacity: 0.8;
+    }
+  }
+`;
+
+const Error = styled.span`
+  font-weight: 600;
+  color: tomato;
+`;
 
 const CreateUser = () => {
-  return <div>CreateUser</div>;
+  const [isLoading, setLoading] = useState(false);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const onChange = (e) => {
+    //이벤트 객체를 분리함 ( target -> name/value )
+    //console.log(e);
+    const { name, value } = e.target;
+    if (name === "name") {
+      setName(value);
+    } else if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+
+  //console.log(name, email, password);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    //회원가입 실행
+    try {
+      // create an account
+      // set the name of the user.
+      // redirect to the home page
+    } catch (e) {
+      // setError
+    } finally {
+      setLoading(false);
+    }
+    console.log(name, email, password);
+  };
+  return (
+    <Wrapper>
+      <Title>Log into 𝕏</Title>
+      <Form onSubmit={onSubmit}>
+        <Input
+          onChange={onChange}
+          name="name"
+          value={name}
+          placeholder="Name"
+          type="text"
+          required
+        />
+        <Input
+          onChange={onChange}
+          name="email"
+          value={email}
+          placeholder="Email"
+          type="email"
+          required
+        />
+        <Input
+          onChange={onChange}
+          value={password}
+          name="password"
+          placeholder="Password"
+          type="password"
+          required
+        />
+        <Input
+          type="submit"
+          value={isLoading ? "Loading..." : "Create Account"}
+        />
+      </Form>
+      {error !== "" ? <Error>{error}</Error> : null}
+    </Wrapper>
+  );
 };
 
 export default CreateUser;
