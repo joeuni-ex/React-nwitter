926352981f5f6d9f9412e642cc26bfd3018c6956
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2023-12-08 11:18:34 +0900

    로그아웃 버튼 추가

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import { auth } from "../firebase";
 
 const Home = () => {
-  return <div>Home</div>;
+  //로그아웃 함수
+  const logOut = () => {
+    auth.signOut();
+  };
+  return (
+    <h1>
+      <button onClick={logOut}>Log Out</button>
+    </h1>
+  );
 };
 
 export default Home;
