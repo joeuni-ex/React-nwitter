9ca66725f32b9f38932039eb36fdecd9ae8f37ea
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2023-12-08 10:25:37 +0900

    파이어 베이스 인증 추가 및 준비

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
 import { useEffect, useState } from "react";
 import LoadingScreen from "./components/LoadingScreen";
+import { auth } from "./firebase";
 
 //styled Conponents
 const GlobalStyle = createGlobalStyle`
@@ -52,8 +53,12 @@ function App() {
   const [isLoading, setIsLoading] = useState(true);
   const init = async () => {
     //파이어베이스 체크
-    setTimeout(() => setIsLoading(false), 2000);
+    //2초동안 로딩한 다음 loading을 false시킨다.
+    //setTimeout(() => setIsLoading(false), 2000);
+    await auth.authStateReady();
+    setIsLoading(false);
   };
+  //한 번만 실행한다.
   useEffect(() => {
     init();
   }, []);
@@ -61,6 +66,7 @@ function App() {
   return (
     <>
       <GlobalStyle />
+      {/* 로딩이면 로딩스크린을 보여주고 아니면 router를 보여준다 */}
       {isLoading ? <LoadingScreen /> : <RouterProvider router={router} />}
     </>
   );
--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
@@ -10,3 +11,6 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+
+//파이어베이스 인증
+export const auth = getAuth();
